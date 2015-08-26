7434d9f91405b1e66030f77d02cb21155e0f2505
Steven Abrams <steven.abrams86@gmail.com>
2015-08-25 22:17:32 -0600

    working concept

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,21 @@
 app.controller('ChatController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
-  
+  var conversation = new Firebase('https://sa-todolist.firebaseio.com/chat');
+  $scope.editForm = false;
+  $scope.messages = $firebaseArray(conversation);
+  $scope.newMessage = function () {
+    $scope.messages.$add({username: $scope.username, avatarUrl: $scope.avatarUrl, content: $scope.content}).then(function () {
+      $scope.content = '';
+    })
+  }
+  $scope.removeMessage = function (message) {
+    $scope.messages.$remove(message);
+  }
+  $scope.editMessage = function () {
+    this.editForm = !this.editForm;
+  }
+  $scope.updateMessage = function (message) {
+    this.message.content = this.messageContent;
+    $scope.messages.$save(this.message);
+    this.editForm = false;
+  }
 }])
